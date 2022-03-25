be35032e5171f36f929d4b013dc8eb8bead9bef5
Derik Daniel Daza <Daniel@MacBook-Pro-de-Oscar-2.local>
2022-03-25 01:10:11 -0500

    add initial files

---
+++ src/main/java/com/test/myusersapp/MyusersappApplication.java
@@ -0,0 +1,13 @@
+package com.test.myusersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyusersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyusersappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/myusersapp/MyusersappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.myusersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyusersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
