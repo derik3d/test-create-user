de089123feef06cf7ded05e76c64800708766c86
Derik Daniel Daza <Daniel@ddazanisumcom.local>
2022-03-25 13:10:31 -0500

    add testing

--- src/main/java/com/test/myusersapp/dto/user_creation_request/CreateUserRequest.java
+++ src/main/java/com/test/myusersapp/dto/user_creation_request/CreateUserRequest.java
@@ -1,16 +1,19 @@
 package com.test.myusersapp.dto.user_creation_request;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequest {
     @NotBlank(message = "Name is mandatory")
     String name;
@@ -21,5 +24,6 @@ public class CreateUserRequest {
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,10}$", message = "not valid, at 8 to 10 characters, at least one letter and one number")
     String password;
     @Valid
+    @NotNull(message = "A phone at least is mandatory")
     List<CreatePhoneRequest> phones;
 }
--- src/test/java/com/test/myusersapp/MyusersappApplicationTests.java
+++ src/test/java/com/test/myusersapp/MyusersappApplicationTests.java
@@ -1,13 +1,107 @@
 package com.test.myusersapp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.test.myusersapp.dto.user_creation_request.CreateUserRequest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.ArrayList;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class MyusersappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Test
+    void contextLoads() throws Exception {
+
+    }
+
+    @Test
+    void testPostUserEndpointOk() throws Exception {
+
+        CreateUserRequest createUserRequest = new CreateUserRequest("Dani", "Dani@el.com", "1234567i", new ArrayList<>());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(createUserRequest);
+
+        ResultActions perform0 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON).content(json)).andExpect(status().is2xxSuccessful());
+
+    }
+
+
+    @Test
+    void testPostUserEndpointAlreadyUsedEmail() throws Exception {
+
+        CreateUserRequest createUserRequest = new CreateUserRequest("DaniDaza", "DaniDaza@el.com", "1234567i", new ArrayList<>());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(createUserRequest);
+
+        ResultActions perform0 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON).content(json)).andExpect(status().is2xxSuccessful());
+
+        ResultActions perform1 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON).content(json)).andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    void testPostUserEndpointMissingFields() throws Exception {
+
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        CreateUserRequest createUserRequest = new CreateUserRequest("", "Dani@el.com", "1234567i", new ArrayList<>());
+        String json = objectMapper.writeValueAsString(createUserRequest);
+        ResultActions perform0 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON).content(json)).andExpect(status().is4xxClientError());
+
+
+        CreateUserRequest createUserRequest1 = new CreateUserRequest("DaniDaza", "", "1234567i", new ArrayList<>());
+        json = objectMapper.writeValueAsString(createUserRequest1);
+        ResultActions perform1 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON).content(json)).andExpect(status().is4xxClientError());
+
+        CreateUserRequest createUserRequest2 = new CreateUserRequest("DaniDaza", "Dani@el.com", "", new ArrayList<>());
+        json = objectMapper.writeValueAsString(createUserRequest2);
+        ResultActions perform2 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON).content(json)).andExpect(status().is4xxClientError());
+
+        CreateUserRequest createUserRequest3 = new CreateUserRequest("DaniDaza", "Dani@el.com", "1234567i", null);
+        json = objectMapper.writeValueAsString(createUserRequest3);
+        ResultActions perform3 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON).content(json)).andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    void testPostUserEndpointNoBodyExpects4xx() throws Exception {
+
+        ResultActions perform0 = mvc.perform(post("/api/v1/user").contentType(
+                MediaType.APPLICATION_JSON)).andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void testTestGetEndpointWelcomeOk() throws Exception {
+
+        ResultActions perform0 = mvc.perform(get("/api/v1/welcome").contentType(
+                MediaType.APPLICATION_JSON)).andExpect(status().isOk());
+    }
+
 
 }
